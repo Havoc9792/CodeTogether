a7858a2f84240e3f78e739baf339e8f83ce25290
TinoChow9792 <tinochow9792@gmail.com>
2015-03-21 15:24:14 +0800

    Run Against TestCase Fixed
    test cases pass correctly now,timeout 3s is added

--- code/java.sh
+++ code/java.sh
@@ -4,6 +4,8 @@ rm $2/output.txt
 rm $2/debug/runtime.txt
 touch $2/output.txt
 touch $2/debug/runtime.txt
+touch $2/tempoutput.txt
+chmod 777 $2/tempoutput.txt
 chmod 777 $2/output.txt
 chmod 777 $2/debug/runtime.txt
 	
@@ -15,12 +17,18 @@ for c in $(cat $2/input.txt)
 do
 echo $c > $2/temp.txt
 echo "Result for Test Case $counter with input(s) : $c \n" >> $2/output.txt
-java -classpath $2 $file 0< $2/temp.txt 1>> $2/output.txt 2> $2/debug/errorplaceholder.txt
-if [ -s $2/debug/errorplaceholder.txt]
+timeout 3s java -classpath $2 $file 0< $2/temp.txt 1> $2/tempoutput.txt 2> $2/debug/errorplaceholder.txt
+if [ -z $2/debug/errorplaceholder.txt && -z $2/tempoutput.txt ]
 then
-	cat $2/debug/errorplaceholder.txt >> $2/debug/runtime.txt
-	cat $2/debug/errorplaceholder.txt >> $2/output.txt
-	
+	echo "EXECUTION TIMEOUT,IT TAKES MORE THAN 3S TO EXECUTE IT!" >> $2/output.txt
+else
+	if [ -s $2/debug/errorplaceholder.txt]
+	then
+		cat $2/debug/errorplaceholder.txt >> $2/debug/runtime.txt
+		cat $2/debug/errorplaceholder.txt >> $2/output.txt
+	else
+		cat $2/tempoutput.txt >> $2/output.txt
+	fi
 fi
 counter=$((counter+1))
 done
--- code/testcase.sh
+++ code/testcase.sh
@@ -12,18 +12,20 @@ chmod 777 $testFolder/output.txt
 chmod 777 $testFolder/runtime.txt
 
 
+touch $testFolder/input.txt
 touch $testFolder/temp.txt
 touch $testFolder/error.txt
+echo $input > $testFolder/input.txt
+echo "MainClass : $2 \nInput : $3 \nExpectedOutput : $4" >> $testFolder/temp.txt
+timeout 3s java -classpath $groupFolder $mainClass 0<$testFolder/input.txt 1> $testFolder/output.txt 2> $testFolder/error.txt
 
-timeout 2s java -classpath $groupFolder $mainClass $input 1> $testFolder/output.txt 2> $testFolder/error.txt;
-
-actualOuput=$(cat $testFolder/output.txt)
-if(-z "$actualOuput")
+actualOutput=$(cat $testFolder/output.txt)
+if [ -z "$actualOutput" ]
 then
 	echo -n "TIMEOUT"
 else
 
-	if("$actualOuput" == "$expectedOuput")
+	if [ "$actualOutput" = "$expectedOutput" ]
 	then
 		echo -n "PASS"
 	else
