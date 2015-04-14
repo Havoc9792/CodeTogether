d18372ca696ac691bc2e29a87ce298fea29cc776
mokkahei24 <mokkahei24@gmail.com>
2015-04-14 13:40:46 +0800

    [MainClass] Bug Fixed for mainClass.sh

--- code/mainClass.sh
+++ code/mainClass.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env sh
-for classfile in "$@/*.class"; do
+#echo `ls $@/*.class`
+for classfile in `ls $@/*.class`; do
+	#echo $classfile | cut -f 1 -d '.'
     classname=$(echo $classfile | cut -f 1 -d '.')
-	#echo $classname
-    if javap -public $classname.class | fgrep 'void main'; then
+	#echo $classname	
+    if (javap -public $classname.class | fgrep 'void main' ); then
         echo "@@@@@@@@@@$classname"
     fi
+    #echo 1
 done
