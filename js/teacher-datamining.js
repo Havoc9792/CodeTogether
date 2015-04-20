3b02b286617de588ac624d5ab7a403ca56927be6
mokkahei24 <mokkahei24@gmail.com>
2015-04-20 21:11:23 +0800

    [UI] Fix

--- js/teacher-datamining.js
+++ js/teacher-datamining.js
@@ -5,11 +5,11 @@ $(function(){
 		//alert(assignment_id);
 		$.getJSON("/datamining/", {input: input, assignment_id: assignment_id}, function(res){
 			console.log(res);
-			//$(".overlay2").fadeOut();
+			$(".overlay2").fadeOut();
 			var content = "";
 			for(var i=0; i<res.length; i++){
 				content += "Label: " + res[i][0] + "<br />";
-				content += "Probability: " + res[i][1] + "<br /><br />";		
+				content += "Probability: " + res[i][1] + "% <br /><br />";		
 			}			
 			$(".pro").html(content);
 			console.log(content, res);
